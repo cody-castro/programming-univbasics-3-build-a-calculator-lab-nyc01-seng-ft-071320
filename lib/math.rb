be06a83c07acc96470ee5478012f6e47e0ca85a4
Cody C <codyicastro@gmail.com>
2020-06-26 16:39:10 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,6 @@
 def addition(num1, num2)
-  puts "#{num1} + #{num2}" 
+  puts "#{num1} + #{num2}"
+  return Sum
 end
 
 def subtraction(num1, num2)
@@ -7,17 +8,24 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  puts "#{num1} / #{num2}" 
 end
 
 def multiplication(num1, num2)
-
+  puts "#{num1} * #{num2}" 
 end
 
 def modulo(num1, num2)
-
+  puts "#{num1} % #{num2}" 
 end
 
-def square_root(num)
-
+def square_root(** num)
+  puts "#{num1}"
 end
+
+addition(5,4)
+subtraction(10,5)
+division(100, 25)
+multiplication(7,2)
+modulo(7,14)
+square_root(7)
